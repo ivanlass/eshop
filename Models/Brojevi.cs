8f79fde821c8087fb278d95572ea3bd0cf79fb50
Marko Jovanovic <anubis1909@yahoo.com>
2019-11-30 21:50:38 +0100

    brojevi za test

---
+++ Models/Brojevi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Brojevi
+{
+	public Brojevi()
+	{
+        int n = 10;
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Brojevi za test od 1 do 10 {n}");
+        }
+    }
+}
